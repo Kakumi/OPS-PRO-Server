06aecf4fe50bf26529c2e375742d6d98d0573fa2
Damien Brebion <mangasa2000@hotmail.com>
2023-04-30 22:20:55 +0200

    Add logs and exception handler

--- OPS Pro Server/Hubs/RoomHub.cs
+++ OPS Pro Server/Hubs/RoomHub.cs
@@ -11,53 +11,91 @@ namespace OPS_Pro_Server.Hubs
     {
         public async Task<bool> CreateRoom(Guid id, string? password, string? description)
         {
-            var user = _userManager.GetUser(id);
-            if (user != null)
+            try
             {
-                var room = new Room(user)
+                var hasPassword = !string.IsNullOrWhiteSpace(password);
+                _logger.LogInformation("Creating room for user {Id} with password: {HasPassword} and description: {Description}", id, hasPassword, description);
+                var user = _userManager.GetUser(id);
+                if (user != null)
                 {
-                    Password = password,
-                    UsePassword = !string.IsNullOrWhiteSpace(password),
-                    Description = description
-                };
+                    var room = new Room(user)
+                    {
+                        Password = password,
+                        UsePassword = hasPassword,
+                        Description = description
+                    };
 
-                _roomManager.AddRoom(room);
+                    _roomManager.AddRoom(room);
 
-                await Groups.AddToGroupAsync(Context.ConnectionId, room.Id.ToString());
+                    await Groups.AddToGroupAsync(Context.ConnectionId, room.Id.ToString());
 
-                return true;
+                    _logger.LogInformation("Room created. Id: {Id}", room.Id);
+
+                    return true;
+                }
+
+                return false;
+            } catch(Exception ex)
+            {
+                _logger.LogError(ex, ex.Message);
+                return false;
             }
-
-            return false;
         }
 
         public List<Room> GetRooms()
         {
-            //Remove password from room
-            var rooms = _roomManager.GetRooms().Select(x => x.Clone()).ToList();
-            rooms.ForEach(x => x.Password = null);
-            return rooms;
+            try
+            {
+                //Remove password from room
+                var rooms = _roomManager.GetRooms().Select(x => x.Clone()).ToList();
+                rooms.ForEach(x => x.Password = null);
+                return rooms;
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, ex.Message);
+                return new List<Room>();
+            }
         }
         
         public async Task<bool> JoinRoom(Guid id, Guid roomId, string? password)
         {
-            var user = _userManager.GetUser(id);
-            var room = _roomManager.GetRoom(roomId);
-            if (user != null && room != null && room.IsJoinable(user, password))
+            try
             {
-                room.Opponent = user;
-                await Groups.AddToGroupAsync(Context.ConnectionId, roomId.ToString());
-                await Clients.Group(room.Id.ToString()).SendAsync(nameof(IRoomHubEvent.RoomUpdated), room);
-                return true;
-            }
+                _logger.LogInformation("User {Id} try to join room {RoomId}", id, roomId);
+                var user = _userManager.GetUser(id);
+                var room = _roomManager.GetRoom(roomId);
+                if (user != null && room != null && room.IsJoinable(user, password))
+                {
+                    room.Opponent = user;
+                    await Groups.AddToGroupAsync(Context.ConnectionId, roomId.ToString());
+                    await Clients.Group(room.Id.ToString()).SendAsync(nameof(IRoomHubEvent.RoomUpdated), room);
+                    _logger.LogInformation("User {Id} joined room sucessfully", id);
+                    return true;
+                }
 
-            return false;
+                return false;
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, ex.Message);
+                return false;
+            }
         }
 
         public async Task<bool> LeaveRoom(Guid id)
         {
-            var user = _userManager.GetUser(id);
-            return await LeaveRoom(user);
+            try
+            {
+                _logger.LogInformation("User: {Id} try to leave his current room.", id);
+                var user = _userManager.GetUser(id);
+                return await LeaveRoom(user);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, ex.Message);
+                return false;
+            }
         }
 
         protected async Task<bool> LeaveRoom(User? user)
@@ -71,10 +109,12 @@ namespace OPS_Pro_Server.Hubs
 
                     if (room.Creator.Id == user.Id)
                     {
+                        _logger.LogInformation("The leaver is the creator, disolving the room {Id}.", room.Id);
                         await Clients.Group(room.Id.ToString()).SendAsync(nameof(IRoomHubEvent.RoomDeleted));
 
                         if (room.Opponent != null)
                         {
+                            _logger.LogInformation("An opponent was in the room: {Id}, remove the user {Username} from the room.", room.Id, room.Opponent.Username);
                             await Groups.RemoveFromGroupAsync(room.Opponent.ConnectionId, room.Id.ToString());
                         }
 
@@ -82,6 +122,7 @@ namespace OPS_Pro_Server.Hubs
                     }
                     else if (room.Opponent?.Id == user.Id)
                     {
+                        _logger.LogInformation("The leaver is the opponent, remove the user from the room {Id}.", room.Id);
                         room.Opponent = null;
                         room.OpponentReady = false;
                         await Clients.Group(room.Id.ToString()).SendAsync(nameof(IRoomHubEvent.RoomUpdated), room);
@@ -94,28 +135,46 @@ namespace OPS_Pro_Server.Hubs
 
         public async Task<bool> SetReady(Guid userId, bool ready)
         {
-            var user = _userManager.GetUser(userId);
-            if (user != null)
+            try
             {
-                var room = _roomManager.GetRoom(user);
-                if (room != null)
+                var user = _userManager.GetUser(userId);
+                if (user != null)
                 {
-                    if (room.Creator == user)
+                    var room = _roomManager.GetRoom(user);
+                    if (room != null)
                     {
-                        room.CreatorReady = ready;
+                        if (room.Creator == user)
+                        {
+                            _logger.LogInformation("Update status for the creator {UserId} to {Ready}.", userId, ready);
+                            room.CreatorReady = ready;
+                        }
+                        else
+                        {
+                            _logger.LogInformation("Update status for the opponent {UserId} to {Ready}.", userId, ready);
+                            room.OpponentReady = ready;
+                        }
+
+                        await Clients.Group(room.Id.ToString()).SendAsync(nameof(IRoomHubEvent.RoomUpdated), room);
+
+                        return ready;
                     }
                     else
                     {
-                        room.OpponentReady = ready;
+                        _logger.LogInformation("Failed to update ready status for the user {UserId}, room doesn't exist.", userId);
                     }
-
-                    await Clients.Group(room.Id.ToString()).SendAsync(nameof(IRoomHubEvent.RoomUpdated), room);
-
-                    return ready;
                 }
-            }
+                else
+                {
+                    _logger.LogInformation("Failed to update ready status for the user {UserId}, user doesn't exist.", userId);
+                }
 
-            return false;
+                return false;
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, ex.Message);
+                return false;
+            }
         }
     }
 }
--- OPS Pro Server/Hubs/UserHub.cs
+++ OPS Pro Server/Hubs/UserHub.cs
@@ -10,21 +10,36 @@ namespace OPS_Pro_Server.Hubs
     {
         public Guid Register(string username)
         {
-            var guid = Guid.NewGuid();
-            _logger.LogInformation($"Register new user {username} ({guid})");
-            var user = new User(guid, Context.ConnectionId, username);
+            try
+            {
+                var guid = Guid.NewGuid();
+                _logger.LogInformation($"Register new user {username} ({guid})");
+                var user = new User(guid, Context.ConnectionId, username);
 
-            _userManager.AddUser(user);
+                _userManager.AddUser(user);
 
-            return guid;
+                return guid;
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, ex.Message);
+                return Guid.Empty;
+            }
         }
 
         public override async Task OnDisconnectedAsync(Exception? exception)
         {
-            var user = _userManager.GetUser(Context.ConnectionId);
-            _logger.LogInformation("User {Username} disconnected ({Id})", user?.Username, user?.Id);
+            try
+            {
+                var user = _userManager.GetUser(Context.ConnectionId);
+                _logger.LogInformation("User {Username} disconnected ({Id})", user?.Username, user?.Id);
 
-            await LeaveRoom(user);
+                await LeaveRoom(user);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, ex.Message);
+            }
         }
     }
 }
